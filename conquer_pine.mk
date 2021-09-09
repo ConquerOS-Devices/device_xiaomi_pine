693dfdd05cdb63bc48e4a794081a5b6e04c8bae4
Chuck439 <firmankediri123@gmail.com>
2021-09-09 08:34:54 +0000

    pine: Update fingerprint to Redfin September
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- conquer_pine.mk
+++ conquer_pine.mk
@@ -27,5 +27,11 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
+
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/pine/pine:10/QKQ1.191014.001/V12.0.2.0.QCMMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
